41648c9a6c641f3847c3f7af84288843652d30ea
liujiehong <nicolastinkl@gmail.com>
2013-12-27 14:56:06 +0800

    no errors

---
+++ xianchangjia/Extend/LXNetManager/LXChatDBStoreManager.h
@@ -0,0 +1,13 @@
+//
+//  LXChatDBStoreManager.h
+//  laixin
+//
+//  Created by apple on 13-12-27.
+//  Copyright (c) 2013年 jijia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LXChatDBStoreManager : NSObject
+
+@end
---
+++ xianchangjia/Extend/LXNetManager/LXChatDBStoreManager.m
@@ -0,0 +1,13 @@
+//
+//  LXChatDBStoreManager.m
+//  laixin
+//
+//  Created by apple on 13-12-27.
+//  Copyright (c) 2013年 jijia. All rights reserved.
+//
+
+#import "LXChatDBStoreManager.h"
+
+@implementation LXChatDBStoreManager
+
+@end
