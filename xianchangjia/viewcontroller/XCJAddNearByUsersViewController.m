52f1dc5a676bc92796f50aaf9c3d6e54ddbeb04b
liujiehong <nicolastinkl@gmail.com>
2014-01-11 19:17:41 +0800

    创建田家附近的人的界面

---
+++ xianchangjia/viewcontroller/XCJAddNearByUsersViewController.m
@@ -0,0 +1,150 @@
+//
+//  XCJAddNearByUsersViewController.m
+//  laixin
+//
+//  Created by apple on 14-1-11.
+//  Copyright (c) 2014年 jijia. All rights reserved.
+//
+
+#import "XCJAddNearByUsersViewController.h"
+#import "XCAlbumAdditions.h"
+#import "MLNetworkingManager.h"
+
+@interface XCJAddNearByUsersViewController ()
+{
+    NSMutableArray * _datasource;
+}
+@end
+
+@implementation XCJAddNearByUsersViewController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    NSMutableArray  * array = [[NSMutableArray alloc] init];
+    _datasource = array;
+ 
+    [SVProgressHUD showWithStatus:@"正在查找附近的人..."];
+    double delayInSeconds = .5;
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+        /*
+         [[MLNetworkingManager sharedManager] sendWithAction:@"geo.user.search" parameters:@{} success:^(MLRequest *request, id responseObject) {
+         if (responseObject) {
+         NSDictionary * resultDict = responseObject[@"result"];
+         NSArray * array = resultDict[@"users"];
+         [array enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+         LXUser * user = [[LXUser alloc] initWithDict:obj];
+         [_datasource addObject:user];
+         }];
+         }
+         } failure:^(MLRequest *request, NSError *error) {
+         
+         }];
+         */
+    });
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    // Return the number of sections.
+    return 1;
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 60.0f;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+#warning Incomplete method implementation.
+    // Return the number of rows in the section.
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"nearcell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }   
+    else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a story board-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+
+ */
+
+@end
