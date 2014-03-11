9c8362425b8d2d47c1673c05e1455119f9530c59
liujiehong <nicolastinkl@gmail.com>
2014-03-11 19:56:01 +0800

    new change with tinkl

--- xianchangjia/viewcontroller/XCJMutiMMViewController.m
+++ xianchangjia/viewcontroller/XCJMutiMMViewController.m
@@ -7,9 +7,14 @@
 //
 
 #import "XCJMutiMMViewController.h"
+#import "XCAlbumAdditions.h"
+#import "FCUserDescription.h"
+#import "XCJAddUserTableViewController.h"
 
-@interface XCJMutiMMViewController ()
-
+@interface XCJMutiMMViewController ()<UIActionSheetDelegate>
+{
+    NSMutableArray * _datasource;
+}
 @end
 
 @implementation XCJMutiMMViewController
@@ -27,11 +32,48 @@
 {
     [super viewDidLoad];
 
+    self.title = @"列表";
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
  
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    
+    
+    NSMutableArray * arrays = [[EGOCache globalCache] plistForKey:KSingerCount];
+    if(arrays.count > 0){
+        _datasource = [arrays mutableCopy];
+        
+        UIBarButtonItem *rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"清空" style:UIBarButtonItemStyleDone target:self action:@selector(clearCacheClick:)];
+        self.navigationItem.rightBarButtonItem = rightBarButtonItem;
+    }
+        
+    if (arrays.count  == 0) {
+        [self showErrorText:@"没有数据"];
+    }
+}
+
+- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    if (buttonIndex == 0) {
+        
+        [[EGOCache globalCache] removeCacheForKey:KSingerCount];
+        double delayInSeconds = 0.1;
+        dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+        dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+            [[NSNotificationCenter defaultCenter] postNotificationName:@"updateMyKSonger" object:@"remove"];
+        });
+        
+        [_datasource removeAllObjects];
+        [self.tableView reloadData];
+    }
+}
+
+-(IBAction)clearCacheClick:(id)sender
+{
+    UIActionSheet * sheet = [[UIActionSheet alloc] initWithTitle:@"确定清空吗" delegate:self cancelButtonTitle:@"取消" destructiveButtonTitle:@"清空" otherButtonTitles:nil, nil];
+    [sheet showInView:self.view];
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -44,26 +86,81 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-#warning Potentially incomplete method implementation.
-    // Return the number of sections.
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-#warning Incomplete method implementation.
     // Return the number of rows in the section.
-    return 0;
+    return _datasource.count;
+}
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 58.0f;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    static NSString *CellIdentifier = @"Cell";
+    static NSString *CellIdentifier = @"myCell";
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    UIImageView * imageview =  (UIImageView * )[cell.contentView subviewWithTag:1];
+    UILabel * name =  (UILabel * )[cell.contentView subviewWithTag:2];
+    UILabel * des =  [cell.contentView subviewWithTag:3];
+    UIButton * butondel =  (UIButton *)[cell.contentView subviewWithTag:4];
+    NSString * userid   = _datasource[indexPath.row];
+    [[[LXAPIController sharedLXAPIController] requestLaixinManager] getUserDesPtionCompletion:^(id response, NSError *error) {
+        FCUserDescription * user = response;
+        NSString *Urlstring = [tools getUrlByImageUrl:user.headpic Size:100];
+        [imageview setImageWithURL:[NSURL URLWithString:Urlstring] placeholderImage:[UIImage imageNamed:@"aio_ogactivity_default"]];
+        name.text = user.nick;
+        des.text = user.signature.length<=0?@"Ta没有写任何东西":user.signature;
+        
+    } withuid:userid];
+    butondel.tag = indexPath.row;
     
-    // Configure the cell...
+    [butondel addTarget:self action:@selector(delClick:) forControlEvents:UIControlEventTouchUpInside];
+    
     
     return cell;
+}
+
+-(IBAction)delClick:(id)sender
+{
+    UIButton * button = sender;
+
+    NSString * userid = _datasource[button.tag];
+    NSMutableArray * array = [[[EGOCache globalCache] plistForKey:KSingerCount] mutableCopy];
+ 
+    //        NSMutableArray * array = [NSMutableArray arrayWithArray:oldarray];
+    if (array) {
+        if ([array containsObject:userid]) {
+            //如果存在 就移除
+            [array removeObject:userid];
+            
+            [[EGOCache globalCache] setPlist:array forKey:KSingerCount];
+            
+            double delayInSeconds = 0.1;
+            dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+            dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+                [[NSNotificationCenter defaultCenter] postNotificationName:@"updateMyKSonger" object:@"remove"];
+            });
+
+        }
+    }
+    [_datasource removeObjectAtIndex: button.tag];
+    [self.tableView reloadData];
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    NSString * userid   = _datasource[indexPath.row];
+    [[[LXAPIController sharedLXAPIController] requestLaixinManager] getUserDesPtionCompletion:^(id response, NSError *error) { 
+        XCJAddUserTableViewController * addUser = [self.storyboard instantiateViewControllerWithIdentifier:@"XCJAddUserTableViewController"];
+        addUser.UserInfo = response;
+        [self.navigationController pushViewController:addUser animated:YES];
+    } withuid:userid];
+ 
 }
 
 /*
