09fa6ee880b74fcc4af3c535700411b5151ab05d
nicolastinkl <nicolastinkl@gmail.com>
2014-04-25 15:34:13 +0800

    create pods

---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :ios, "7.0"
+
+pod 'JLRoutes', '~> 1.5'
+pod 'ReactiveCocoa', '~> 2.3'
+pod 'ReactiveViewModel', '~> 0.2'
+
+target :laixinTests, :exclusive => true do
+  # pod 'Kiwi'
+end
---
+++ Pods/Pods-environment.h
@@ -0,0 +1,38 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// JLRoutes
+#define COCOAPODS_POD_AVAILABLE_JLRoutes
+#define COCOAPODS_VERSION_MAJOR_JLRoutes 1
+#define COCOAPODS_VERSION_MINOR_JLRoutes 5
+#define COCOAPODS_VERSION_PATCH_JLRoutes 0
+
+// ReactiveCocoa
+#define COCOAPODS_POD_AVAILABLE_ReactiveCocoa
+#define COCOAPODS_VERSION_MAJOR_ReactiveCocoa 2
+#define COCOAPODS_VERSION_MINOR_ReactiveCocoa 3
+#define COCOAPODS_VERSION_PATCH_ReactiveCocoa 0
+
+// ReactiveCocoa/Core
+#define COCOAPODS_POD_AVAILABLE_ReactiveCocoa_Core
+#define COCOAPODS_VERSION_MAJOR_ReactiveCocoa_Core 2
+#define COCOAPODS_VERSION_MINOR_ReactiveCocoa_Core 3
+#define COCOAPODS_VERSION_PATCH_ReactiveCocoa_Core 0
+
+// ReactiveCocoa/no-arc
+#define COCOAPODS_POD_AVAILABLE_ReactiveCocoa_no_arc
+#define COCOAPODS_VERSION_MAJOR_ReactiveCocoa_no_arc 2
+#define COCOAPODS_VERSION_MINOR_ReactiveCocoa_no_arc 3
+#define COCOAPODS_VERSION_PATCH_ReactiveCocoa_no_arc 0
+
+// ReactiveViewModel
+#define COCOAPODS_POD_AVAILABLE_ReactiveViewModel
+#define COCOAPODS_VERSION_MAJOR_ReactiveViewModel 0
+#define COCOAPODS_VERSION_MINOR_ReactiveViewModel 2
+#define COCOAPODS_VERSION_PATCH_ReactiveViewModel 0
+
