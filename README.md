88d395e6e3de50e76c019a643200023e122a85c0
nicolastinkl <nicolastinkl@gmail.com>
2014-04-25 15:27:02 +0800

    del all

--- README.md
+++
@@ -1,9 +0,0 @@
-laixin
-======
-
-
-![image](http://images.cnblogs.com/cnblogs_com/tinkl/253133/o_[W_8L3YMIXH`0EN1FU__O$M.jpg)
-
-
-
-![image](http://blog.leezhong.com/image/FRP_ReactiveCocoa_large.png)
--- xianchangjia/Additions/BTSCoreGraphics.m
+++
@@ -1,7 +0,0 @@
-//
-//  Copyright (c) 2011 Brian Coyner. All rights reserved.
-//
-
-extern void BTSPrintCurrentCTM(CGContextRef context); 
-extern void BTSDrawPoint(CGContextRef context, CGPoint point);
-extern void BTSDrawCoordinateAxes(CGContextRef context);
